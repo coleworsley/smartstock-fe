48927e0ae68caaebc24a0b1b2a866b0b87d802a8
coleworsley <cole.s.worsley@gmail.com>
2017-11-12 10:43:19 -0700

    Add actions for pulling quotes

---
+++ src/actions/quotes.js
@@ -0,0 +1,38 @@
+export const quotesLoading = (loading) => {
+  return {
+    type: 'QUOTES_LOADING',
+    loading,
+  }
+}
+
+export const quoteError = (error) => {
+  return {
+    type: 'QUOTE_ERROR',
+    error,
+  }
+}
+
+export const quotes = (data) => {
+  return {
+    type: 'QUOTE',
+    data,
+  }
+}
+
+export const fetchQuotes = (symbols) => {
+  return (dispatch) => {
+    dispatch(quotesLoading(true));
+
+    fetch(`/api/v1/quotes?${symbols}`)
+      .then((res) => {
+        dispatch(quotesLoading(false))
+        return res.json();
+      })
+      .then(({data}) => {
+        return dispatch(quotes(data))
+      })
+      .catch((error) => {
+        return dispatch(quoteError(error))
+      })
+  }
+}
